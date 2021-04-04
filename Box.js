32fa5c0b90c90c36966d55a256c2cf0ebcd88e58
jayant13-gif <74774520+jayant13-gif@users.noreply.github.com>
2021-04-04 17:53:27 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,17 @@
+class Box{
+    constructor(){
+    var options={
+        restitution:1,
+ }
+
+    this.body = Bodies.rectangle(200,100,50,50,options);
+    World.add(world,this.body);}
+    display() {
+        var pos = this.body.position;
+        rectMODE(CENTER);
+        fill(255);
+        rect(pos.x,pos.y,this.innerWidth,this.height);
+        }
+
+    
+}
